549980b191a387e259a8b4148674eff5ffe16b5e
atharvashelke7 <shelkeatharva07@gmail.com>
2025-09-17 23:43:49 +0530

    four sub new

--- four-sub.js
+++ four-sub.js
@@ -1,16 +1,26 @@
-function fourSub(arr) {
+function fourSub(arr, target) {
+  arr.sort((a, b) => a - b);
   let set = new Set();
 
   for (let i = 0; i < arr.length; i++) {
+    if (i > 0 && arr[i] === arr[i - 1]) continue;
     for (let j = i + 1; j < arr.length; j++) {
-      for (let k = j + 1; k < arr.length; k++) {
-        for (let l = k + 1; l < arr.length; l++) {
-          let sum = arr[i] + arr[j] + arr[k] + arr[l];
-          if (sum === 0) {
-            let temp = [arr[i], arr[j], arr[k], arr[l]];
-            temp.sort((a, b) => a - b);
-            set.add(temp.toString());
-          }
+      if (j > i + 1 && arr[j] === arr[j - 1]) continue;
+      let k = j + 1;
+      let l = arr.length - 1;
+      while (k < l) {
+        let sum = arr[i] + arr[j] + arr[k] + arr[l];
+        if (sum === target) {
+          let temp = [arr[i], arr[j], arr[k], arr[l]];
+          set.add(temp.join(","));
+          k++;
+          l--;
+          while (k < l && arr[k] === arr[k - 1]) k++;
+          while (k < l && arr[l] === arr[l + 1]) l--;
+        } else if (sum < target) {
+          k++;
+        } else {
+          l--;
         }
       }
     }
@@ -19,4 +29,4 @@ function fourSub(arr) {
   return Array.from(set).map((str) => str.split(",").map(Number));
 }
 
-console.log(fourSub([-1, -1, -1, 2, 2, 0, 0, 0]));
+console.log(fourSub([-1, -1, -1, 2, 2, 0, 0, 0], 0));
